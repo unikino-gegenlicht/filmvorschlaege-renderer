1ec279d46d102ba6aadf8249cb25068c0f901da2
Jan Eike Suchard <jan@suchard.cloud>
2024-04-20 12:59:59 +0200

    removing unnecessary print of value

--- types/survey-monkey-time.go
+++ types/survey-monkey-time.go
@@ -1,14 +1,12 @@
 package types
 
 import (
-	"fmt"
 	"time"
 )
 
 type SMTime time.Time
 
 func (t *SMTime) UnmarshalJSON(data []byte) error {
-	fmt.Println(string(data))
 	realTime, err := time.Parse(`"2006-01-02 15:04:05"`, string(data))
 	if err != nil {
 		return err
