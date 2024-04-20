cdcc4b93e90002ac475ac195eb42a495ea0fd7a1
Jan Eike Suchard <jan@suchard.cloud>
2024-04-20 11:54:41 +0200

    removing weasy dependency check

--- init.go
+++ init.go
@@ -1,31 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"time"
-
 	"github.com/pterm/pterm"
 )
 
-var dependencies []string = []string{"weasyprint"}
-
 func init() {
 	printHeader()
-	checkDependencies()
-}
-
-func checkDependencies() {
-	spinner, _ := pterm.DefaultSpinner.Start("Checking dependencies")
-	time.Sleep(1000 * time.Millisecond)
-	for _, dependency := range dependencies {
-		if !commandExists(dependency) {
-			spinner.Fail(fmt.Sprintf(`"%[1]s" is not installed. please install "%[1]s"`, dependency))
-			os.Exit(1)
-		}
-		time.Sleep(500 * time.Millisecond)
-	}
-	spinner.Success("Required dependencies found")
 }
 
 func printHeader() {
