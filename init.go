b249f080a59534692d7d553169b05b21254c9919
Jan Eike Suchard <jan@suchard.cloud>
2025-01-03 21:20:31 +0100

    auto extracting blacklist

--- init.go
+++ init.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/pterm/pterm"
 )
 
+var blacklistFileName string
+
 func init() {
 	printHeader()
+
+	file, err := os.CreateTemp("", "distributors*")
+	defer file.Close()
+	if err != nil {
+		pterm.DefaultLogger.Fatal("unable to extract distributor list")
+		os.Exit(1)
+	}
+
+	file.WriteString(distributorBlacklist)
+
+	blacklistFileName = file.Name()
+
 }
 
 func printHeader() {
